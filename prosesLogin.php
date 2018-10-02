e18793f712aa2bc08deb4c48adaceb25725629ee
gilangerlangga90 <42725308+gilangerlangga90@users.noreply.github.com>
2018-10-02 20:17:37 +0700

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,16 @@
-<?php 
-
-
-
-
-
+<?php  
+session_start();
+error_reporting(0);
+$user = array(
+				"username" => "mahasiswa",
+				"password" => "1234"
+			);
+if (isset($_POST['masuk'])) {
+	if ($_POST['username'] == $user['username'] && $_POST['password'] == $user['password']){
+		$_SESSION['username'] = $_POST['username'];
+		header("Location: registrasi.php");
+	}else{
+		echo "Username atau Password Salah!";
+	}
+}
 ?>
